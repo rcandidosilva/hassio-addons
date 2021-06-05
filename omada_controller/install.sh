6164c4382681d7dc4e52b18e037d033df8485737
Rodrigo Cândido da Silva <rcandidosilva@gmail.com>
2021-06-05 20:58:29 -0300

    Initial commit

--- omada_controller/install.sh
+++ omada_controller/install.sh
@@ -43,6 +43,7 @@ apt-get remove --autoremove mongodb-org
 rm /etc/apt/sources.list.d/mongodb*.list
 apt-get update
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 4B7C549A058F8B6B
+echo "deb [arch=amd64] http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 apt-get update
 apt-get install --no-install-recommends -y "${PKGS[@]}"
